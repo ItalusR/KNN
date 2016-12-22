3fc6f1eb06f7ff954520fe9fdbb4eecfcfe9c662
ItalusR <italus.prado@hotmail.com>
2016-12-22 00:50:26 -0300

    media and deviation

--- KNN/ViewController.swift
+++ KNN/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
         let textFromFile = self.readFromFile(withFile: "iris")
         splitValues(withText: textFromFile)
         
-        for index in 0...9{
+        for _ in 0...9{
             self.hits.append(0)
             self.flowerNumbers = [Int]()
             self.arrayFlowers = [Flower]()
@@ -36,9 +36,34 @@ class ViewController: UIViewController {
             self.chooseFlowersToTest(withQuantity: 30)
             self.compareFlowersTest()
             test += 1
-            print("Acertou \(Double(self.hits[index])/30*100)% dos testes")
+            //print("Acertou \(Double(self.hits[index])/30*100)% dos testes")
         }
+        //print("Média geral = \(Double(self.hits.reduce(0, +))/300*100)%")
+        self.mediaAndStandardDeviation()
+    }
+    
+    // Media and Standard Deviation of the tests
+    func mediaAndStandardDeviation(){
+        var media: CGFloat = 0
+        var deviation: CGFloat = 0
+        var preVariation = [CGFloat]()
         
+        // Media
+        for number in hits {
+            media += CGFloat(number)
+        }
+        media = media/CGFloat(self.hits.count)
+        print("Media: \(media)")
+        
+        // Standard Deviation
+        for number in self.hits{
+            preVariation.append(CGFloat(number)-media)
+            preVariation[preVariation.count-1] *= preVariation.last!
+        }
+        var variation: CGFloat = preVariation.reduce(0, +)
+        variation = variation/media
+        deviation = sqrt(variation)
+        print("Desvio padrão = \(deviation)")
     }
     
     // Generate arrayFlower with the data
